de6d5c9520486c84f2ec2507a6a6b9970edd5e82
OriginalVirus <skimoon@qq.com>
2014-09-01 09:43:27 +0800

    Update Bullet.h

--- Classes/Bullet.h
+++ Classes/Bullet.h
@@ -1,26 +1,33 @@
 #pragma once
 #include "cocos2d.h"
 #include "MapLayer.h"
+#include "BaseTank.h"
 USING_NS_CC;
 
 typedef enum {
 	NORMAL = 0,
-	GOLD	// gold bullet
+	GOLD   = 1// gold bullet
 } BulletType;
 
-class Bullet :
-	public Sprite
+class Bullet :public Sprite
+	
 {
+protected:
+	virtual Action * _dieoneAction();  //子弹和子弹碰撞
+	virtual Action* _dietwoAction();   //子弹和其他东西碰撞
 public:
-	Bullet(void);
-	virtual ~Bullet(void);
-public:
+	int demage;
 	BulletType _type;	// NORMAL
 	float _speed;
-	Action* _dieAction;
+	 //死亡动画
 	// Action* _moveAction;
 public:
-	void die();
-	void move();
+	virtual bool initWithFile(std::string name ,BaseTank * tank,float speed);
+	Bullet * create(std::string name ,BaseTank * tank,float speed);
+	//死亡方法
+	void die();  
+	//移动方法
+	void move(){}
+	
+	
 };
-
