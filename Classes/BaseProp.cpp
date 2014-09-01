4421f05cb57a77bb60f8bc8f94a348538bb74627
OriginalVirus <skimoon@qq.com>
2014-09-01 09:37:26 +0800

    Update BaseProp.cpp

--- Classes/BaseProp.cpp
+++ Classes/BaseProp.cpp
@@ -1,11 +1,16 @@
 #include "BaseProp.h"
 
 
-BaseProp::BaseProp(void)
-{
+Vec2  BaseProp::randomPosition(){
+  Size size = Director::getInstance()->getVisibleSize();
+	float x = CCRANDOM_0_1()* size.width;
+	float y = CCRANDOM_0_1()* size.height;
+	return  Vec2(x,y);
 }
 
-
-BaseProp::~BaseProp(void)
-{
+bool BaseProp::initWithFile(std::string name ){
+  if (!Sprite::initWithFile(name)) {
+	  return false;
+	}
+  return true;
 }
