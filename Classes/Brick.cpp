e433c9383badf65aae055f8aa8cc3de9168d63bb
OriginalVirus <skimoon@qq.com>
2014-09-01 10:45:16 +0800

    Update Brick.cpp

--- Classes/Brick.cpp
+++ Classes/Brick.cpp
@@ -1,26 +1,16 @@
-#include "BaseBrick.h"
+#include "Brick.h"
 
 
-<<<<<<< HEAD
 Brick::Brick(BrickType type, Vec2 tilePoint, Sprite* tile) {
 	this->_type = type;
 	this->_tilePoint = tilePoint;
 	this->_tile = tile;
-=======
-BaseBrick::BaseBrick(void)
-{
->>>>>>> b28289224eaae95c46d29de571362caf58b7c3f7
 }
 
 Brick::~Brick(void) {
 
-<<<<<<< HEAD
 }
 
 void Brick::removeFromMap(MapLayer * maplayer) {
 	maplayer->removeBrick(this);
-=======
-BaseBrick::~BaseBrick(void)
-{
->>>>>>> b28289224eaae95c46d29de571362caf58b7c3f7
 }
