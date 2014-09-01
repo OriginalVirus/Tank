fa5000e84486f96614f25742c083e9c4350e5b2f
OriginalVirus <skimoon@qq.com>
2014-09-01 10:50:48 +0800

    Update JoystickLayer.cpp

--- Classes/JoystickLayer.cpp
+++ Classes/JoystickLayer.cpp
@@ -1,11 +1,25 @@
 #include "JoystickLayer.h"
+#include "JoystickPad.h"
 
+#define PAD_RADIUS 128/2
 
-JoystickLayer::JoystickLayer(void)
-{
+JoystickLayer* JoystickLayer::create(MainTank * tank) {
+	auto jsl = new JoystickLayer();
+	if (jsl && jsl->init(tank)) {
+		jsl->autorelease();
+		return jsl;
+	}
+	CC_SAFE_DELETE(jsl);
+	return NULL;
 }
 
+bool JoystickLayer::init(MainTank* tank) {
+	if (!Layer::init()) return false;
 
-JoystickLayer::~JoystickLayer(void)
-{
+	auto pad = JoystickPad::createWithFile("direction.png", PAD_RADIUS);
+	pad->_mainTank = tank;
+	pad->setPosition(PAD_RADIUS + 10, PAD_RADIUS + 10);
+	this->addChild(pad);
+
+	return true;
 }
