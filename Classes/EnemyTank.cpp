cfc00ca02c53d724b3502e473ebeda55cdff5187
OriginalVirus <skimoon@qq.com>
2014-09-01 10:25:36 +0800

    Update EnemyTank.cpp

--- Classes/EnemyTank.cpp
+++ Classes/EnemyTank.cpp
@@ -3,19 +3,18 @@
 #include "GameScene.h"
 #include "BaseTank.h"
 bool EnemyTank::init(){
-	if (!BaseTank::init())
-	{
-		return false;
-	}
+	if (!BaseTank::init()) return false;
+	
 	isDie = false;
 	this->schedule(schedule_selector(EnemyTank::fire),2);
+	
 	return true;
-	
-	
 }
+
 void EnemyTank::ai(float dt){
 	
 }
+
 void EnemyTank::fire(float t){
 	auto bullet = Bullet::create();
 	auto scene = GameScene::getInstance();
