d3eebabd5c34c8e45e7d956f84778e3dca477d1e
OriginalVirus <skimoon@qq.com>
2014-09-01 10:36:04 +0800

    Update MainTank.cpp

--- Classes/MainTank.cpp
+++ Classes/MainTank.cpp
@@ -1,17 +1,19 @@
 #include "MainTank.h"
 #include "Bullet.h"
 #include "GameScene.h"
+
 bool MainTank::init(){
 	if (!Sprite::initWithFile("p2tankU.png"))
 	{
 		return false;
 	}
 	_hp = 1;
-	_speed = 50;
+	_speed = 8;
 	_current_life = 4;
-
+	isDie = false;
 	return true;
 }
+
 void MainTank::fire(float t){
 	auto bullet = Bullet::create();
 	auto scene = GameScene::getInstance();
