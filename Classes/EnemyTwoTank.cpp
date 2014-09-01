36a6704ef93bb4374ed17eb84571b5e2b922206a
OriginalVirus <skimoon@qq.com>
2014-09-01 10:32:53 +0800

    Update EnemyTwoTank.cpp

--- Classes/EnemyTwoTank.cpp
+++ Classes/EnemyTwoTank.cpp
@@ -1,12 +1,10 @@
 #include "EnemyTwoTank.h"
 #include "BaseTank.h"
 bool EnemyTwoTank::init(){
-	if (!Sprite::initWithFile("enemy2D.png"))
-	{
-		return false;
-	}
-	isDie(false);
+	if (!Sprite::initWithFile("enemy2D.png")) return false;
+		
 	_hp = 3;
-	_speed = 50;
+	_speed = 8;
+	
 	return true;
 }
