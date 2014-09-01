143db072e768c95b77de36625965c19fb1b343e6
OriginalVirus <skimoon@qq.com>
2014-09-01 09:38:30 +0800

    Update BaseProp.cpp

--- Classes/BaseProp.cpp
+++ Classes/BaseProp.cpp
@@ -2,15 +2,15 @@
 
 
 Vec2  BaseProp::randomPosition(){
-  Size size = Director::getInstance()->getVisibleSize();
+	Size size = Director::getInstance()->getVisibleSize();
 	float x = CCRANDOM_0_1()* size.width;
 	float y = CCRANDOM_0_1()* size.height;
 	return  Vec2(x,y);
 }
 
 bool BaseProp::initWithFile(std::string name ){
-  if (!Sprite::initWithFile(name)) {
-	  return false;
+	if (!Sprite::initWithFile(name)) {
+		return false;
 	}
-  return true;
+	return true;
 }
