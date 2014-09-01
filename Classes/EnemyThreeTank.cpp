40a4e97d0ec2614a5a8cf44b92ebd65e07eace46
OriginalVirus <skimoon@qq.com>
2014-09-01 10:31:37 +0800

    Update EnemyThreeTank.cpp

--- Classes/EnemyThreeTank.cpp
+++ Classes/EnemyThreeTank.cpp
@@ -1,12 +1,12 @@
 #include "EnemyThreeTank.h"
 #include "BaseTank.h"
+
 bool EnemyThreeTank::init(){
-	if (!Sprite::initWithFile("enemy3D.png"))
-	{
-		return false;
-	}
+	if (!Sprite::initWithFile("enemy3D.png")) return false;
+	
 	isDie(false);
 	_hp = 1;
-	_speed = 150;
+	_speed = 24;
+	
 	return true;
 }
